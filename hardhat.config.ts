d739ff68d30d4245dfde2f6f524230860cb826eb
mackeee-orange <makino.aaa@gmail.com>
2022-08-23 01:54:33 +0900

    test: Tokenのテストとconfig

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,22 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+// import { HardhatUserConfig } from "hardhat/config";
+// import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-waffle";
 
-const config: HardhatUserConfig = {
+const config = {
   solidity: "0.8.9",
+  networks: {
+    matic: {
+      url: process.env.MUMBAI_RPC || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : []
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "JPY",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
 };
 
 export default config;
---
+++ test/Token.ts
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+import {ethers} from "hardhat";
+
+describe("Token contract", () => {
+  it("Deployment should assign the total supply of tokens to the owner", async () => {
+    const [owner] = await ethers.getSigners();
+    const Token = await ethers.getContractFactory("Token");
+    const hardhatToken = await Token.deploy();
+    const ownerBalance = await hardhatToken.balanceOf(owner.address);
+    expect(await hardhatToken.totalSupply()).to.eq(ownerBalance);
+  })
+})
