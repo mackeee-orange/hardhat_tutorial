570456c6195111ae28c444eb71ee3a48503e20c6
mackeee-orange <makino.aaa@gmail.com>
2022-08-23 02:13:32 +0900

    feat: deployできるまで

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,14 @@
 // import { HardhatUserConfig } from "hardhat/config";
 // import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-waffle";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config = {
   solidity: "0.8.9",
   networks: {
-    matic: {
+    mumbai: {
       url: process.env.MUMBAI_RPC || "",
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : []
     },
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,16 +1,12 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
   const Token = await ethers.getContractFactory("Token");
   const token = await Token.deploy();
 
   await token.deployed();
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${token.address}`);
+  console.log(`deployed to ${token.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
